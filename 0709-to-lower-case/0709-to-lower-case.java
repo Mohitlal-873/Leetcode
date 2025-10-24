f03d037f5acc34dd56ff0fc54d8af4a1df232794
Mohitlal Das <mohititer@gmail.com>
2025-10-24 21:33:39 +0530

    Time: 0 ms (100%), Space: 41.8 MB (43.01%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+       StringBuilder newStr=new StringBuilder();
+       for(char ch : s.toCharArray()){
+         if(ch>='A' && ch<='Z'){
+            newStr.append((char)(ch+32));
+         }
+         else{
+            newStr.append(ch);
+         }
+
+       }
+       return newStr.toString();
     }
 }
